e007fb204b4d0ad32e7ece5564e7523224bd7f92
Laura Jaime <laura.jv@coditramuntana.com>
2021-02-11 11:36:27 +0100

    Change some overrides with Deface

--- app/overrides/add_custom_error_messages_in_authorization_modals.rb
+++ app/overrides/add_custom_error_messages_in_authorization_modals.rb
@@ -1,15 +1,15 @@
 unless ActiveModel::Type::Boolean.new.cast(ENV['DOCKER'])
   Deface::Override.new(virtual_path: 'decidim/authorization_modals/_content',
-                     name: 'add_custom_error_messages_in_authorization_modals',
-                     replace: '<li>',
-                     text: '
-                      <% case field %>
-                        <% when :birthdate %>
-                          <li> <strong> <%= text_invalid_age_authorizer(status) %> </strong> </li>
-                        <% when :subcensus %>
-                          <li> <strong> <%= t(".subcensus.invalid_field", scope: "diba")%> </strong> </li>
-                        <% else %>
-                          <li><strong><%= t ".#{status.code}.invalid_field", field: t("#{status.handler_name}.fields.#{field}", scope: "decidim.authorization_handlers"), value: value ? "(#{value})" : "" %></strong></li>
-                        <% end %>
-                     ')
+                       name: 'add_custom_error_messages_in_authorization_modals',
+                       replace: '<li>',
+                       text: '
+                        <% case field %>
+                          <% when :birthdate %>
+                            <li> <strong> <%= text_invalid_age_authorizer(status) %> </strong> </li>
+                          <% when :subcensus %>
+                            <li> <strong> <%= t(".subcensus.invalid_field", scope: "diba")%> </strong> </li>
+                          <% else %>
+                            <li><strong><%= t ".#{status.code}.invalid_field", field: t("#{status.handler_name}.fields.#{field}", scope: "decidim.authorization_handlers"), value: value ? "(#{value})" : "" %></strong></li>
+                          <% end %>
+                       ')
 end
