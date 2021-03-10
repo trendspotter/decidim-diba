c1e07b101fdc5d84d05324c5d0a255ffa45d70a3
LauraJaimeC <75725233+LauraJaimeC@users.noreply.github.com>
2021-03-10 09:16:17 +0100

    Upgrade to Decidim v0.23 (#96)
    * Upgrade to Decidim v0.20
    
    * Update social share buttons
    
    * DOC: Added upgrade instructions
    
    * Upgrade to Ruby 2.7.2
    
    * Resort Decidim dependencies in Gemfile
    
    * Upgrade to Decidim v0.20
    
    * Update social share buttons
    
    * DOC: Added upgrade instructions
    
    * Upgrade to Ruby 2.7.2
    
    * Resort Decidim dependencies in Gemfile
    
    * Upgrade to Decidim v0.21
    
    * Change some overrides with Deface
    
    * Upgrade passenger-ruby docker image to ruby 2.7
    
    * DOC: update docs in how to upgrade Ruby.
    
    * Do not run deface overrides while generating the docker image
    
    Beacuse at assets:precompile it crashes as term-cusomizer tries to access the database
    
    * Remove and comment geocoder lines
    
    * Change some overrides with Deface
    
    * Upgrade to Decidim v0.22
    
    * Remove key in translation
    
     * That key not exists in decidim translations
    
    * Do not force ssl from docker containers
    
    * Changes in override doc
    
    * Fix sidkiq file
    
    * Change some overrides with Deface
    
    * Upgrade to Decidim v0.22
    
    * Remove key in translation
    
     * That key not exists in decidim translations
    
    * Upgrade to Decidim v0.23
    
    * Change maps configuration
    
    * Add debates and comments command to index in doc
    
    * Fix migration so that it does not raise exception
    
    Co-authored-by: Oliver Valls <199462+tramuntanal@users.noreply.github.com>

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -30,7 +30,7 @@ Decidim.configure do |config|
   # Geocoder configuration
   # geocoder_config = Rails.application.secrets.geocoder
   # if geocoder_config[:here_app_id].present? && geocoder_config[:here_app_code].present?
-  #   config.geocoder = {
+  #   config.maps = {
   #     here_app_id: geocoder_config[:here_app_id],
   #     here_app_code: geocoder_config[:here_app_code]
   #   }
