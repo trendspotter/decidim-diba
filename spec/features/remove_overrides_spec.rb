5e1ba0fbcc3945f2b0937c8ee2b7b288b73dc3b6
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2021-04-12 16:16:27 +0200

    Add CSV exporter override to take the headers from all proposals

--- spec/features/remove_overrides_spec.rb
+++ spec/features/remove_overrides_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe "Overrides" do
     # remove app/permissions/decidim/initiatives/permissions.rb, see docs/overrides.md for details
     expect(Decidim.version).to be < "0.22"
   end
+
+  scenario "remove override export proposals to csv in all locales" do
+    # remove config/initializers/csv_exporter.rb if PR https://github.com/decidim/decidim/pull/7825 is backported to 0.24
+    # otherwise remove the override after upgrading to 0.25
+    expect(Decidim.version).to be < "0.24"
+  end
 end
